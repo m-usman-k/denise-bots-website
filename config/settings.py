b796f43c2b71e483b8da30afbd380354542f8cc9
Muhammad Usman <usmank.personal@outlook.com>
2026-05-15 17:13:27 +0500

    Fixes

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import environ
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
